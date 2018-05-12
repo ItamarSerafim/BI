c0837db8e9eaffcc754f9c852c3beafd5c39e089
Itamar Serafim Silva <saladatododia@hotmail.com>
2018-05-12 02:34:56 -0300

    Create generate-data-draft.js

---
+++ generate-data-draft.js
@@ -0,0 +1,16 @@
+/*
+
+I plan to make it helper tool for transforming generated data.
+
+
+https://en.wikipedia.org/wiki/List_of_largest_companies_by_revenue
+
+https://www.generatedata.com/
+ProductName,SalesVolume,SalesMan,Client
+
+Excell miner plugin|MVA Word Plugin|Chrome Scrapper plugin|Calculuz|Mobile Calc|Clever Mail|MGF Calendar|Firebase2Excel|Augumented Pen
+Jan|Feb|Mar|Abr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec
+Douglas Adams|James Allen|Babara Adams|Steve Allen|Francis Bacon|Julian Barnes|Carl Becker|Eric Biden|David Bogart
+2016|2017|2018
+Walmart|Toyota|Volkswagen|Apple|Exxon Mobil|AT&T|Amazon|Honda|Allianz
+*/
